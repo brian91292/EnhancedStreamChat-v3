1830210c2929c8035d95e0bb1ce750c94996afd1
brian91292 <brian@yur.fit>
2020-06-11 23:53:46 -0500

    bump version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EnhancedStreamChat")]
-[assembly: AssemblyDescription("rc2")]
+[assembly: AssemblyDescription("rc5")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EnhancedStreamChat")]
